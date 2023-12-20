dadd295652a2e4831822086a7177bc904b41589a
yongha <yong1807@gmail.com>
2023-12-20 16:14:36 +0900

    최초 커밋

---
+++ src/main/java/kr/co/gudi/HoonyMusicApplication.java
@@ -0,0 +1,13 @@
+package kr.co.gudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoonyMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoonyMusicApplication.class, args);
+	}
+
+}
---
+++ src/main/java/kr/co/gudi/main/MainController.java
@@ -0,0 +1,13 @@
+package kr.co.gudi.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping(value="/")
+	public String login() {
+		return "login";
+	}
+}
