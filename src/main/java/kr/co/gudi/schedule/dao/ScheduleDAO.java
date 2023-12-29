c98ffe57f9896bb176a1380377f84297881751fe
KIMMINJI <jklnm2013@gmail.com>
2023-12-29 10:35:05 +0900

    캘린더 조회출력

--- src/main/java/kr/co/gudi/schedule/dao/ScheduleDAO.java
+++ src/main/java/kr/co/gudi/schedule/dao/ScheduleDAO.java
@@ -1,5 +1,19 @@
 package kr.co.gudi.schedule.dao;
 
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.co.gudi.schedule.dto.ScheduleDTO;
+
+@Mapper
 public interface ScheduleDAO {
+
+	void write(HashMap<String, Object> param);
+
+	List<ScheduleDTO> list();
+
+	String addForm(HashMap<String, Object> params);
 
 }
