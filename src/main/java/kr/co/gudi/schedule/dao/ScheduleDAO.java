6fc03ad9c901077884552a6a3f6b549b7d3e05fa
KIMMINJI <jklnm2013@gmail.com>
2024-01-11 16:54:49 +0900

    충돌 해결

--- src/main/java/kr/co/gudi/schedule/dao/ScheduleDAO.java
+++ src/main/java/kr/co/gudi/schedule/dao/ScheduleDAO.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.co.gudi.schedule.dto.ScheduleDTO;
+
+@Mapper
 public interface ScheduleDAO {
 
 	void write(HashMap<String, Object> param);
--- src/main/java/kr/co/gudi/schedule/service/ScheduleService.java
+++ src/main/java/kr/co/gudi/schedule/service/ScheduleService.java
@@ -5,6 +5,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.gudi.schedule.dao.ScheduleDAO;
+import kr.co.gudi.schedule.dto.ScheduleDTO;
+
+@Service
 public class ScheduleService {
 	Logger logger = LoggerFactory.getLogger(getClass());
 	@Autowired ScheduleDAO dao;
